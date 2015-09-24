92a21168fb16b52193360adf3b1fb3a19e2d997f
Aaron Stannard <iamstannard@gmail.com>
2015-09-25 06:53:22 +1000

    Add HTML, some basic CSS for structure, and JS pseudocode.

---
+++ ga-final-assignment/js/main.js
@@ -0,0 +1,23 @@
+/*global document, $, console*/
+
+(function () {
+    'use strict';
+
+    $(document).ready(function () {
+
+        // side menu
+        // on show menu button click slide menu in
+        // on menu item click scroll to relevant section of page
+        // on close menu button click slide menu out
+        // on go to top button click scroll to top of page
+        
+        // gallery/screenshots (for mobile only?)
+        // on left arrow click slide image out and introduce new screenshot from right
+        // on right arrow click slide image out and introduce new screenshot from left
+        // or other nav?
+        
+        // scroll-based animation?
+        
+    });
+
+}());
