461a1f19a3d0d02de77533e5e15f172e0daf700b
Aaron Stannard <iamstannard@gmail.com>
2015-09-26 18:55:29 +1000

    Small layout tweaks.

--- ga-final-assignment/js/main.js
+++ ga-final-assignment/js/main.js
@@ -7,16 +7,15 @@
 
         // side menu
         // on show menu button click slide menu in
-        // on menu item click scroll to relevant section of page
+        // on menu item click (smooth) scroll to relevant section of page
         // on close menu button click slide menu out
-        // on go to top button click scroll to top of page
+        // on go to top button click (smooth) scroll to top of page
         
-        // gallery/screenshots (for mobile only?)
-        // on left arrow click slide image out and introduce new screenshot from right
-        // on right arrow click slide image out and introduce new screenshot from left
-        // or other nav?
+        // gallery/carousel
+        // http://kenwheeler.github.io/slick/
         
-        // scroll-based animation?
+        // scroll-based animation? maybe
+        // http://www.sitepoint.com/introduction-jquery-scroll-based-animations/
         
     });
 
