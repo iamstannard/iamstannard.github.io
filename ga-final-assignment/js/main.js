50b6d34b792bfad9be80bd828183ce5ec6d1580e
Aaron Stannard <iamstannard@gmail.com>
2015-10-06 21:23:37 +1100

    Add styling, JS, carousel, images, etc.

--- ga-final-assignment/js/main.js
+++ ga-final-assignment/js/main.js
@@ -1,4 +1,4 @@
-/*global document, $, console*/
+/*global document, $, console, alert, window*/
 
 (function () {
     'use strict';
@@ -6,17 +6,74 @@
     $(document).ready(function () {
 
         // side menu
+
+        var sideBarAnimTime = 500;
+
+        function setSideBarLeft() {
+            var windowWidth = $(window).width(),
+                sideBarWidth = $('#side-menu-wrapper').css('width'),
+                sideBarWidthVal = sideBarWidth.match(/\d/g),
+                sideBarLeftVal;
+            sideBarWidthVal = sideBarWidthVal.join("");
+            sideBarLeftVal = windowWidth - sideBarWidthVal;
+            return sideBarLeftVal;
+        }
+
         // on show menu button click slide menu in
-        // on menu item click (smooth) scroll to relevant section of page
+
+        $('#show-menu-btn').click(function () {
+            $('#side-menu-wrapper').animate({
+                left: setSideBarLeft() + 'px'
+            }, sideBarAnimTime);
+
+        });
+
         // on close menu button click slide menu out
-        // on go to top button click (smooth) scroll to top of page
-        
-        // gallery/carousel
+
+        $('#hide-menu-btn').click(function () {
+            $('#side-menu-wrapper').animate({
+                left: $(window).width() + 'px'
+            }, sideBarAnimTime);
+        });
+
+        // ----- smooth scroll to element -----
+
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+
+        // screenshot/carousel
         // http://kenwheeler.github.io/slick/
-        
-        // scroll-based animation? maybe
-        // http://www.sitepoint.com/introduction-jquery-scroll-based-animations/
-        
+
+        $('.screenshot-carousel').slick({
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+            arrows: true,
+            responsive: [
+                {
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true,
+                        arrows: false
+                    }
+                }
+            ]
+        });
+
     });
 
 }());
